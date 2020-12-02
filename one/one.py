e96987093132302f41f41e730f87a4008960e18b
Hedy Li <hedyhyry@gmail.com>
2020-12-02 06:51:28 +0000

    better one.py

--- one/one.py
+++ one/one.py
@@ -1,8 +1,27 @@
-with open('inputs.txt') as f:
-    li = [int(i.strip()) for i in f.readlines()]
-
 from itertools import combinations
 
-for com in combinations(li, 3):
-    if sum(com) == 2020:
-        print(com[0] * com[1] * com[2])
+
+def load_list(fname = "one.txt"):
+    with open(fname) as f:
+        li = [int(i.strip()) for i in f.readlines()]
+    return li
+
+
+def a():
+    global li
+    for com in combinations(li, 2):
+        if sum(com) == 2020:
+            print(com[0] * com[1])
+
+
+def b():
+    global li
+    for com in combinations(li, 3):
+        if sum(com) == 2020:
+            print(com[0] * com[1] * com[2])
+
+
+if __name__  == "__main__":
+    li = load_list()
+    a()
+    b()
