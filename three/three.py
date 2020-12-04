46c4d636e635d64a22abe3e70f0ff2dfcd7bacd8
Hedy Li <hedyhyry@gmail.com>
2020-12-04 02:55:33 +0000

    Add day three

---
+++ three/three.py
@@ -0,0 +1,23 @@
+with open("three.txt") as f:
+    li = [list(i.strip()) for i in f.readlines()]
+
+
+
+def count_trees(right, down):
+    count = x = y = 0
+
+    for y in range(0, len(li), down):
+        if li[y][x % len(li[0])] == '#':
+            # print(li[y], x, li[y][x])
+            count += 1
+        x += right
+    return count
+
+def one():
+    return count_trees(3, 1)
+
+def two():
+    return count_trees(1,1) * count_trees(3,1) * count_trees(5,1) * count_trees(7,1) * count_trees(1,2)
+
+print(one())
+print(two())
