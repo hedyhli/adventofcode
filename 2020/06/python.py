127e95517a2c978d6af710db3feca563cc358f9c
hedy <hedy@tilde.cafe>
2022-12-02 13:44:30 +0800

    Add 2020 Day 6

---
+++ 2020/06/python.py
@@ -0,0 +1,51 @@
+from aocd import submit
+
+
+with open("input.txt") as f:
+    inp = f.read()
+
+
+count = 0
+for group in inp.split('\n\n'):
+    ques = set(group.replace('\n', ''))
+    count += len(ques)
+    # print(ques)
+
+# submit(count, part="a", year=2020, day=6)
+
+
+# count = 0
+# for group in inp.split('\n\n'):
+#     qlist = []
+#     for question in set(group.split('\n')[0]):
+#         qlist.append(question)
+#     newqlist = qlist.copy()
+#     # loop through people other than the first
+#     for person in group.split('\n')[1:]:
+#         for question in newqlist:
+#             nextperson = False
+#             print(qlist)
+#             if question not in person:
+#                 qlist.pop(qlist.index(question))
+#                 nextperson = True
+#                 break
+#             if nextperson:
+#                 break
+#     count += len(qlist)
+# print(count)
+
+count = 0
+for group in inp.split('\n\n'):
+    group = group.rstrip()
+    if '\n' not in group:
+        # only 1 person
+        count += len(set(group))
+        # print(group, count)
+        continue
+    people = [ set(i) for i in group.split('\n') ]
+    # print(people, len(people[0].intersection(*people[1:])))
+    count += len(people[0].intersection(*people[1:]))
+    # print(count)
+
+print(count)
+submit(count, part="b", year=2020, day=6)
