7e07dbe059395df40486351034cafabf38de6005
hedy <hedy@tilde.cafe>
2022-12-02 18:15:18 +0800

    Finish shell solution for 2022 Day 2 parts 1 and 2

--- 2022/02/shell.sh
+++ 2022/02/shell.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
+# answers have been checked
+
 
 mapscore () { echo $1 | tr ABC 123; }
-mapXYZ () { echo $1 | tr XYZ ABC; }
+# mapXYZ () { echo $1 | tr XYZ ABC; }
 towin () { echo $1 | tr CAB ABC; }
 tolose () { echo $1 | tr CAB BCA; }
 
@@ -13,7 +15,8 @@ score=0
 while read line; do
     col1=$( echo "$line" | grep -o '[ABC]' )
     col2=$( echo "$line" | grep -o '[XYZ]' )
-    abc=$(mapXYZ $col2)
+    abc=$(echo $col2 | tr XYZ ABC)
+
     if [ $abc = $col1 ]; then
         # draw
         score=$((score + 3))
@@ -25,3 +28,29 @@ while read line; do
     score=$((score + $(mapscore $abc)))
 done < input.txt
 echo $score
+# aoc -y 2022 -d 2 submit 1 $score
+
+# part 2
+# getscore () { echo $1 | tr XYZ 036; }
+
+score=0
+while read line; do
+    col1=$( echo "$line" | grep -o '[ABC]' )
+    col2=$( echo "$line" | grep -o '[XYZ]' )
+
+    # score=$((score + $(getscore $col2)))
+    score=$((score + $(echo $col2 | tr XYZ 036)))
+
+    shape=$col1  # Assume draw first
+    case $col2 in
+        X)
+            # lose
+            shape=$(tolose $col1);;
+        Z)
+            # win
+            shape=$(towin $col1);;
+    esac
+    score=$((score + $(mapscore $shape)))
+done < input.txt
+echo $score
+# aoc -y 2022 -d 2 submit 2 $score
