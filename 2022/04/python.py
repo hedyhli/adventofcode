77eb97d19b9465e48ac8c7b679251f51ffb4dcea
hedy <hedy@tilde.cafe>
2022-12-04 18:11:23 +0800

    Refactor python solution of 2022 day 4

--- 2022/04/python.py
+++ 2022/04/python.py
@@ -1,31 +1,13 @@
+import re
+
 from aocd import submit
 
 with open("input.txt") as f:
-    lines = f.read().strip().split('\n')
+    lines = [ list(map(int, re.split(",|-", l))) for l in f.read().strip().split('\n') ]
 
-
-n = 0
-for line in lines:
-    a, b = line.split(',')
-    a1, a2 = (int(i) for i in a.split('-'))
-    b1, b2 = (int(i) for i in b.split('-'))
-    if (a1 >= b1 and a2 <= b2) or (b1 >= a1 and b2 <= a2):
-        n += 1
-        # print(line, a1, a2, b1, b2)
-
+n = len(list(filter(lambda l: (l[0] >= l[2] and l[1] <= l[3]) or (l[2] >= l[0] and l[3] <= l[1]), lines)))
 print(n)
 submit(n, year=2022, part="a", day=4)
-
-n = 0
-for line in lines:
-    a, b = line.split(',')
-    a1, a2 = (int(i) for i in a.split('-'))
-    aset = set(range(a1, a2+1))
-    b1, b2 = (int(i) for i in b.split('-'))
-    bset = set(range(b1, b2+1))
-    if len(aset & bset) > 0:
-        n += 1
-        # print(line)
-
+n = len(list(filter(lambda l: (len(set(range(l[0], l[1]+1)) & set(range(l[2], l[3]+1))) > 0), lines)))
 print(n)
 submit(n, year=2022, part="b", day=4)
