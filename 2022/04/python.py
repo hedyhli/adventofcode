62eb085026a4d7e70b9029a05c96d6038d264e1e
hedy <hedy@tilde.cafe>
2022-12-04 18:40:18 +0800

    Refactor python solution for 2022 day 4 even more

--- 2022/04/python.py
+++ 2022/04/python.py
@@ -5,9 +5,14 @@ from aocd import submit
 with open("input.txt") as f:
     lines = [ list(map(int, re.split(",|-", l))) for l in f.read().strip().split('\n') ]
 
-n = len(list(filter(lambda l: (l[0] >= l[2] and l[1] <= l[3]) or (l[2] >= l[0] and l[3] <= l[1]), lines)))
+# wtf?
+n = len(list(filter(lambda l: (l[0]<=l[2]<=l[3]<=l[1] or l[2]<=l[0]<=l[1]<=l[3]), lines)))
 print(n)
 submit(n, year=2022, part="a", day=4)
-n = len(list(filter(lambda l: (len(set(range(l[0], l[1]+1)) & set(range(l[2], l[3]+1))) > 0), lines)))
+n = len(list(filter(lambda l: (l[0]<=l[3]>=l[2]<=l[1]), lines)))
 print(n)
 submit(n, year=2022, part="b", day=4)
+
+# old implementation
+# n = len(list(filter(lambda l: (l[0] >= l[2] and l[1] <= l[3]) or (l[2] >= l[0] and l[3] <= l[1]), lines)))
+# n = len(list(filter(lambda l: (len(set(range(l[0], l[1]+1)) & set(range(l[2], l[3]+1))) > 0), lines)))
