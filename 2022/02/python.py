d2698e4e828a860d2b9a0e3284ce7253003ca2b3
hedy <hedy@tilde.cafe>
2022-12-05 12:19:18 +0800

    HARDCODING IS FOR THE WEAK
    (wip) refactor 2022 Day 2 python solution

--- 2022/02/python.py
+++ 2022/02/python.py
@@ -2,62 +2,26 @@ from aocd import submit
 
 
 with open("input.txt") as f:
-    lines = f.readlines()
+    lines = [ list(map(ord, list(line.strip()))) for line in f ]
 
-
-shape = {
-    'A': 1,
-    'B': 2,
-    'C': 3,
-}
-
-m = {
-    'X': 'A',
-    'Y': 'B',
-    'Z': 'C',
-}
-
-# how to win
-win = {
-    'C': 'A',
-    'A': 'B',
-    'B': 'C',
-}
-#...and how to lose
-lose = {
-    'C': 'B',
-    'A': 'C',
-    'B': 'A',
-}
-
-score = 0
-for r in lines:
-    if m[r[2]] == r[0]:
-        score += 3
-    if win[r[0]] == m[r[2]]:
-        score += 6
-    score += shape[m[r[2]]]
-
+score = sum((3,6,0)[(r[2]-r[0])%20%3]+r[2]-87 for r in lines)
 print(score)
 submit(score, part="a", year=2022, day=2)
 
 # part 2
 score = 0
-pts = {
-    'X': 0,
-    'Y': 3,
-    'Z': 6,
-}
 for r in lines:
-    score += pts[r[2]]
-
-    if r[2] == 'Y':
-        score += shape[r[0]]
-    if r[2] == 'Z':
-        score += shape[win[r[0]]]
-    if r[2] == 'X':
-        # lose
-        score += shape[lose[r[0]]]
+    score += r[2]*3-264
+    if r[2] == 88:
+        score += (2,0,1)[r[0]-65]+1
+    if r[2] == 89:
+        score += r[0]-64
+    if r[2] == 90:
+        score += (1,2,0)[r[0]-65]+1
 
 print(score)
 submit(score, part="b", year=2022, day=2)
+
+
+## HARDCODING IS RETARDED
+## HARDCODING IS FOR THE WEAK!!!
