add35b53085157142a3b281ad83264bdca14aac2
hedy <hedy@tilde.cafe>
2022-12-05 12:54:02 +0800

    Refactor python solution for 2022 Day 2

--- 2022/02/python.py
+++ 2022/02/python.py
@@ -2,22 +2,22 @@ from aocd import submit
 
 
 with open("input.txt") as f:
-    lines = [ list(map(ord, list(line.strip()))) for line in f ]
+    lines = [ tuple(map(ord, list(line.strip()))) for line in f ]
 
-score = sum((3,6,0)[(r[2]-r[0])%20%3]+r[2]-87 for r in lines)
+score = sum((3,6,0)[(x-a)%20%3]+x-87 for a,_,x in lines)
 print(score)
 submit(score, part="a", year=2022, day=2)
 
 # part 2
 score = 0
-for r in lines:
-    score += r[2]*3-264
-    if r[2] == 88:
-        score += (2,0,1)[r[0]-65]+1
-    if r[2] == 89:
-        score += r[0]-64
-    if r[2] == 90:
-        score += (1,2,0)[r[0]-65]+1
+for a,_,x in lines:
+    score += x*3-264
+    if x == 88:
+        score += (2,0,1)[a-65]+1
+    if x == 89:
+        score += a-64
+    if x == 90:
+        score += (1,2,0)[a-65]+1
 
 print(score)
 submit(score, part="b", year=2022, day=2)
