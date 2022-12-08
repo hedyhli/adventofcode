e7c5ccfe6777d582f13917133680cf76b733eb7a
hedy <hedy@tilde.cafe>
2022-12-08 11:08:39 +0800

    2022-07 (lua): Better algorithm for finding smallest directory to delete

--- 2022/07/lua.lua
+++ 2022/07/lua.lua
@@ -20,18 +20,17 @@ end
 
 sum = 0 -- for Part One
 should_free = 30000000 - 70000000 + dirs["/"]
-candidates = {} -- for Part Two
+min_del = 70000000 -- for Part Two
 for _, size in pairs(dirs) do
     if size <= 100000 then
         sum = sum + size
     end
-    if size >= should_free then
-        table.insert(candidates, size)
+    if size >= should_free and size < min_del then
+        min_del = size
     end
 end
 print("Total size of all dirs <= 100,000")
 print(sum)
 
-table.sort(candidates)
 print("Smallest dir size to be deleted to have 30,000,000 free space")
-print(candidates[1])
+print(min_del)
