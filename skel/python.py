b721bc6a71a65b3b3469094165cf3527d51a0045
hedy <hedy@tilde.cafe>
2022-12-22 16:41:44 +0800

    Better template for python

--- skel/python.py
+++ skel/python.py
@@ -1,12 +1,37 @@
+# https://adventofcode.com/YEAR/day/DAY
+#
+## USAGE ##
+# Run with 'input.txt' and submit with aocd:
+#     python3 python.py
+# Run with 'test.txt' and don't submit:
+#     python3 python.py test.txt
+# Run with stdin and don't submit:
+#     cat myinput | python3 python.py -
+#     aoc d -y YEAR -d DAY -oi /dev/stdout | python3 python.py -
+#     aocd DAY YEAR | python3 python.py -
 
 
-from aocd import submit
 
-with open("input.txt") as f:
+if __name__ == '__main__':
+
+    import sys
+    inputfn = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+
+    from aocd import submit
+
+    with open(0 if inputfn == '-' else inputfn) as f:
+        lines = f.read().splitlines()
 
 
 
+    # part 1
+    print()
+    if inputfn == "input.txt":
+        submit(, "a", day=DAY, year=YEAR)
 
-submit( , "a", DAY, YEAR)
 
-submit( , "b", DAY, YEAR)
+
+    # part 2
+    print()
+    if inputfn == "input.txt":
+        submit(, "b", day=DAY, year=YEAR)
