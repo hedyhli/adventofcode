4e9d144565b0595f9d4109cd3a70d58cf830aeea
hedy <hedy@tilde.cafe>
2023-12-02 14:35:26 +0800

    update

--- 2015/01/python.py
+++ 2015/01/python.py
@@ -1,4 +1,4 @@
-# https://adventofcode.com/YEAR/day/DAY
+# https://adventofcode.com/2015/day/1
 #
 ## USAGE ##
 # Run with 'input.txt' and submit with aocd:
@@ -9,8 +9,8 @@
 #     python3 python.py test.txt
 # Run with stdin and don't submit:
 #     cat myinput | python3 python.py -
-#     aoc d -y YEAR -d DAY -oi /dev/stdout | python3 python.py -
-#     aocd DAY YEAR | python3 python.py -
+#     aoc d -y 2015 -d 1 -oi /dev/stdout | python3 python.py -
+#     aocd 1 2015 | python3 python.py -
 #     pbpaste | python3 python.py -
 
 # from functools import reduce
@@ -25,18 +25,29 @@ if __name__ == '__main__':
 
     with open(0 if inputfn == '-' else \
               ('example.txt' if inputfn == 'e' else inputfn)) as f:
-        lines = f.read().splitlines()
+        inp = f.read().strip()
 
+
+    floor = 0
+    for ch in inp:
+        floor += 1 if ch == '(' else -1
 
 
     # part 1
-    print()
-    # if inputfn == "input.txt":
-    #     submit(, "a", day=DAY, year=YEAR, reopen=False)
+    print(floor)
+    if inputfn == "input.txt":
+        submit(floor, "a", day=1, year=2015, reopen=False)
 
+    floor = 0
+    for c in range(len(inp)):
+        ch = inp[c]
+        floor += 1 if ch == '(' else -1
+        if floor == -1:
+            break
 
 
     # part 2
-    print()
-    # if inputfn == "input.txt":
-    #     submit(, "b", day=DAY, year=YEAR, reopen=False)
+    c += 1
+    print(c)
+    if inputfn == "input.txt":
+        submit(c, "b", day=1, year=2015, reopen=False)
