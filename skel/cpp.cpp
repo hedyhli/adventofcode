cd2bfa0a2f7ee60ffb51a57a8e6369fc92f7f686
hedy <hedy@tilde.cafe>
2023-12-07 12:48:59 +0800

    update cpp skel

---
+++ skel/cpp.cpp
@@ -0,0 +1,52 @@
+/* Copyright all wrongs deserved */
+/* C++17
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+/* int: -2^31 .. 2^31-1  (2E+9) (32 bits)
+ * ll:  -2^63 .. 2^63-1  (9E+18) (64 bits)
+ * double: +- -1.7E+308
+ * char,bool: 8 bits
+ */
+
+#define endl '\n'
+
+#define debug(x) cerr <<"["<<(#x)<<"="<<(x)<<"]"
+#define debugln(x) cerr <<"["<<(#x)<<"="<<(x)<<"]\n"
+
+#define cendl cout << endl
+#define println(X) cout << X << endl
+
+/* #define max3(X, Y, Z) max(X, max(Y, Z)) */
+/* #define min3(X, Y, Z) min(X, min(Y, Z)) */
+
+#define showvec(a) for(int i=0;i<a.size();i++) cout<<a[i]<<' ';cout<<endl;
+/* #define show1d(a) for(auto const &x : (a)) cerr<<x<<' ';cerr<<endl; */
+
+/* void iswap(int *a,int *b) {int c=*a; *a=*b; *b=c;} */
+
+/* const double PI = 3.141592653589793238462643383279; */
+/* const ll INF = numeric_limits<ll>::max(); */
+/* const int INFI = numeric_limits<int>::max(); */
+/* #define StreamMax numeric_limits<streamsize>::max() */
+
+
+/// parsing ///////////////////////////////////////////////////////////////////
+/// algorithm /////////////////////////////////////////////////////////////////
+/// solution //////////////////////////////////////////////////////////////////
+
+void part1() {
+}
+
+void part2() {
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+
+    part1();
+    part2();
+}
