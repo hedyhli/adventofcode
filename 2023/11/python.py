59540f0a52029cd372c5a166a560c4a62b12e4d2
hedy <hedy@tilde.cafe>
2023-12-12 20:45:40 +0800

    2023-11: Update inefficient python sol

--- 2023/11/python.py
+++ 2023/11/python.py
@@ -13,8 +13,40 @@
 #     aocd 11 2023 | python3 python.py -
 #     pbpaste | python3 python.py -
 
-EXPAND_FACTOR1 = 2
-EXPAND_FACTOR2 = 1_000_000
+FACTOR1 = 2
+FACTOR2 = 1_000_000
+
+def solve(lines: list[str]) -> tuple[int, int]:
+    space = []; space2 = []
+    col_empty = [ True ] * len(lines[0])
+    galaxies = []
+    for i, line in enumerate(lines):
+        space.append([])
+        space2.append([])
+        expand_row = int('#' not in line)
+        for j, c in enumerate(line):
+            space[-1].append(max(1, expand_row * FACTOR1))
+            space2[-1].append(max(1, expand_row * FACTOR2))
+            if c == '#':
+                col_empty[j] = False
+                galaxies.append((i, j))
+
+    for j in range(len(space[0])):
+        if col_empty[j]:
+            for i in range(len(space)):
+                space[i][j] = FACTOR1
+                space2[i][j] = FACTOR2
+
+    s = s2 = 0
+    for g, ga in enumerate(galaxies):
+        for g2 in range(g+1, len(galaxies)):
+            x, y = ga
+            x2, y2 = galaxies[g2]
+            if x > x2: x2, x = x, x2
+            if y > y2: y, y2 = y2, y
+            s += sum(space[i][y] for i in range(x, x2)) + sum(space[x2][i] for i in range(y, y2))
+            s2 += sum(space2[i][y] for i in range(x, x2)) + sum(space2[x2][i] for i in range(y, y2))
+    return s, s2
 
 
 if __name__ == '__main__':
@@ -26,51 +58,11 @@ if __name__ == '__main__':
         lines = f.read().splitlines()
 
     ############################################################################
-    # parse and expand
-    space:  list[list[int]] = []
-    space2: list[list[int]] = []
-    col_empty = [ True ] * len(lines[0])
-    galaxies = []
-    for i, line in enumerate(lines):
-        space.append([])
-        space2.append([])
-        expand_row = int('#' not in line)
-        for j, c in enumerate(line):
-            space[-1].append(max(1, expand_row * EXPAND_FACTOR1))
-            space2[-1].append(max(1, expand_row * EXPAND_FACTOR2))
-            if c == '#':
-                col_empty[j] = False
-                galaxies.append((i, j))
 
-    # expand columns
-    for j in range(len(space[0])):
-        if col_empty[j]:
-            for i in range(len(space)):
-                space[i][j] = EXPAND_FACTOR1
-                space2[i][j] = EXPAND_FACTOR2
-
-    ############################################################################
-    # part 1 & 2
-
-    s = 0
-    s2 = 0
-    for g, ga in enumerate(galaxies):
-        for g2 in range(g+1, len(galaxies)):
-            x, y = ga
-            x2, y2 = galaxies[g2]
-            if x > x2:
-                x2, x = x, x2
-            if y > y2:
-                y, y2 = y2, y
-            s += sum(space[i][y] for i in range(x, x2)) \
-               + sum(space[x2][i] for i in range(y, y2))
-            s2 += sum(space2[i][y] for i in range(x, x2)) \
-                + sum(space2[x2][i] for i in range(y, y2))
-
+    s, s2 = solve(lines)
     print(s)
     if inputfn == "input.txt":
         submit(s, "a", day=11, year=2023, reopen=False)
-
     print(s2)
     if inputfn == "input.txt":
         submit(s2, "b", day=11, year=2023, reopen=False)
